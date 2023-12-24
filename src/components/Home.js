1f187721d80202418630c4531096e0f27b7cb422
Fazeela <fazeelanizam13@gmail.com>
2023-12-24 18:03:58 +0530

    npm link + alt texts

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import be from '../images/be.png';
 import li from '../images/li.png';
 import blog from '../images/blog.png';
 import gh from '../images/gh.png';
+import npm from '../images/npm.png';
 import ReactGA from 'react-ga4';
 
 import { stars } from '../helpers';
@@ -23,16 +24,19 @@ export default function Home() {
         <div className='content'>
           <div className='icons'>
             <a href='https://gist.github.com/fazeelanizam13' target='_blank' rel='noreferrer' title='gists/github'>
-              <img src={gh} alt='' />
+              <img src={gh} alt='github logo' />
+            </a>
+            <a href='https://www.npmjs.com/~fazeelanizam13' target='_blank' rel='noreferrer' title='npm'>
+              <img src={npm} alt='npm logo' />
             </a>
             <a href='https://not-a-tech-blog.web.app/' target='_blank' rel='noreferrer' title='blog'>
-              <img src={blog} alt='' />
+              <img src={blog} alt='chain link icon' />
             </a>
             <a href='https://www.linkedin.com/in/fazeelanizam/' target='_blank' rel='noreferrer' title='linkedin'>
-              <img src={li} alt='' />
+              <img src={li} alt='linkedin logo' />
             </a>
             <a href='https://www.behance.net/fazeelanizam' target='_blank' rel='noreferrer' title='behance'>
-              <img src={be} alt='' />
+              <img src={be} alt='behance logo' />
             </a>
           </div>
         </div>
